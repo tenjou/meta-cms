3d6bd7bc8a296a1eb131d6126950abd16b31c12d
Tenjou <arthur.der.shefer@gmail.com>
2018-09-27 09:02:44 +0300

    Add Select component

---
+++ src/component/Select.js
@@ -0,0 +1,35 @@
+import { component, componentVoid, elementOpen, elementClose, text, store, element } from "wabi"
+
+const Select = component({
+    state: {
+        value: null,
+        src: null
+    },
+
+    mount() {
+        this.props = { onchange: this.handleChange.bind(this) }
+    },
+
+    render() {
+        const options = this.$src
+        if(!this.$value) {
+            this.$value = options[0]
+        }
+
+        elementOpen("select", this.props)
+            for(let n = 0; n < options.length; n++) {
+                const option = options[n]
+                const props = (this.$value === option) ? { value: option, selected: true } : { value: option }
+                elementOpen("option", props)
+                    text(option)
+                elementClose("option")
+            }
+        elementClose("select")
+    },
+
+    handleChange(event) {
+        console.log(event.srcElement.value)
+    }
+})
+
+export default Select
