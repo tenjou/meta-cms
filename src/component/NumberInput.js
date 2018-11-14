8341eb74c26c29b19fe61eca702d3b833fe32589
Tenjou <arthur.der.shefer@gmail.com>
2018-11-15 01:30:19 +0200

    src: support Number min/max states

--- src/component/NumberInput.js
+++ src/component/NumberInput.js
@@ -1,12 +1,23 @@
 import { component, componentVoid, elementOpen, elementClose, text, store, elementVoid } from "wabi"
 
 const NumberInput = component({
+    state: {
+        value: 0,
+        min: Number.MIN_SAFE_INTEGER,
+        max: Number.MAX_SAFE_INTEGER
+    },
+
     mount() {
         this.props = { type: "number", onchange: this.handleChange.bind(this) }
     },
 
     render() {
-        const element = elementVoid("input", this.props).element
+        const element = elementVoid("input", {
+            type: "Number",
+            min: this.$min,
+            max: this.$max,
+            onchange: this.handleChange.bind(this),
+        }).element
         element.value = this.$value
     },
 
