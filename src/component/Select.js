b15be767deea57527ae76fe4eb5020f9217154bb
Tenjou <arthur.der.shefer@gmail.com>
2018-11-28 09:36:55 +0200

    ui: add support for key value pairs in Select component

--- src/component/Select.js
+++ src/component/Select.js
@@ -13,17 +13,40 @@ const Select = component({
 
     render() {
         const options = this.$src
-        if(!this.$value || (options.length === 1 && options[0] !== this.$value)) {
-            this.$value = options[0]
-        }
 
         elementOpen("select", this.props)
-            for(let n = 0; n < options.length; n++) {
-                const option = options[n]
-                const props = (this.$value === option) ? { value: option, selected: true } : { value: option }
-                elementOpen("option", props)
-                    text(option)
-                elementClose("option")
+            if(options.length > 0) {
+                const keyPair = (typeof options[0] === "object") ? true : false
+
+                if(keyPair) {
+                    if(!this.$value || (options.length === 1 && options[0] !== this.$value)) {
+                        this.$value = options[0].value
+                    }        
+
+                    for(let n = 0; n < options.length; n++) {
+                        const option = options[n]
+                        const props = (this.$value === option.value) ? { value: option.value, selected: true } : { value: option.value }
+                        elementOpen("option", props)
+                            text(option.key)
+                        elementClose("option")
+                    }                    
+                }
+                else {
+                    if(!this.$value || (options.length === 1 && options[0] !== this.$value)) {
+                        this.$value = options[0]
+                    }
+
+                    for(let n = 0; n < options.length; n++) {
+                        const option = options[n]
+                        const props = (this.$value === option) ? { value: option, selected: true } : { value: option }
+                        elementOpen("option", props)
+                            text(option)
+                        elementClose("option")
+                    }                    
+                }
+            }
+            else {
+                this.$value = null
             }
         elementClose("select")
     },
