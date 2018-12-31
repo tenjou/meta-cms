52c26265d12362fe66e62f2387c3670f1dfa7b9e
Tenjou <arthur.der.shefer@gmail.com>
2019-01-01 00:39:36 +0200

    update: export removing cache and schemaCache

--- src/service/ExportService.js
+++ src/service/ExportService.js
@@ -2,6 +2,10 @@ import { store } from "wabi"
 
 const create = (options) => {
     const data = options.production ? createProduction(options.named, options.dictionary) : createProject()
+    data.meta.export = {
+        production: options.production
+    }
+    cleanup(data)
     if(options.minify) {
         return JSON.stringify(data)
     }
@@ -43,6 +47,36 @@ const createProduction = (named, dictionary) => {
         }
     }
     return data
+}
+
+const cleanup = (data) => {
+    const production = data.meta.export.production
+    const assets = data.assets
+    for(let key in assets) {
+        const asset = assets[key]
+        if(production) {
+
+        }
+        else {
+            const data = asset.data
+            for(let n = 0; n < data.length; n++) {
+                cleanupAssetItem(data[n])
+            }
+            delete asset.meta.schemaCache
+        }
+    }
+}
+
+const cleanupAssetItem = (item) => {
+    delete item.__cache
+    for(let key in item) {
+        const property = item[key]
+        if(Array.isArray(property)) {
+            for(let n = 0; n < property.length; n++) {
+                cleanupAssetItem(property[n])
+            }
+        }
+    }
 }
 
 export { create }
