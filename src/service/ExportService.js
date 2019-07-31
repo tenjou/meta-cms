31cf064a0e6cb6fa662d04acab038592fd8728d3
Tenjou <arthur.der.shefer@gmail.com>
2019-07-31 20:26:11 +0300

    update: when exporting list with UID export is as dictionary instead

--- src/service/ExportService.js
+++ src/service/ExportService.js
@@ -47,7 +47,7 @@ const createProduction = (named) => {
             for(let n = 0; n < buffer.length; n++) {
                 const item = buffer[n]
                 dataDictionary[item[dictionaryKey]] = item
-                cleanupAssetItem(item)
+                processAssetItem(item, schema)
             }
             buffer = dataDictionary
 		}      
@@ -56,7 +56,7 @@ const createProduction = (named) => {
             for(let n = 0; n < buffer.length; n++) {
                 const item = buffer[n]
                 dataArray[n] = item
-                cleanupAssetItem(item)
+                processAssetItem(item, schema)
             }
             buffer = dataArray
 		}  
@@ -77,30 +77,62 @@ const cleanup = (data, options) => {
         if(options.production) {
             const data = assets[key]
             for(let n = 0; n < data.length; n++) {
-                cleanupAssetItem(data[n])
+                processAssetItem(data[n])
             }
         }
         else {
             const asset = assets[key]
             const data = asset.data
             for(let n = 0; n < data.length; n++) {
-                cleanupAssetItem(data[n])
+                processAssetItem(data[n])
             }
             delete asset.meta.schemaCache
         }
     }
 }
 
-const cleanupAssetItem = (item) => {
-    delete item.__cache
-    for(let key in item) {
-        const property = item[key]
-        if(Array.isArray(property)) {
-            for(let n = 0; n < property.length; n++) {
-                cleanupAssetItem(property[n])
-            }
-        }
-    }
+const processAssetItem = (item, schema) => {
+	delete item.__cache
+	
+	for(let n = 0; n < schema.length; n++) {
+		const propertySchema = schema[n]
+		switch(propertySchema.type) {
+			case "List": {
+				const properties = item[propertySchema.key]
+				const uidKey = getUID(propertySchema.schema)
+				if(uidKey) {
+					const dict = {}
+					item[propertySchema.key] = dict
+
+					for(let n = 0; n < properties.length; n++) {
+						const property = properties[n]
+						processAssetItem(property, propertySchema.schema)
+						dict[property[uidKey]] = property
+					}
+				}
+				else {
+					for(let n = 0; n < properties.length; n++) {
+						processAssetItem(properties[n], propertySchema.schema)
+					}
+				}
+			} break
+
+			case "Type": {
+				const value = item[propertySchema.key]
+				processAssetItem(item, propertySchema.schema[value])
+			} break
+		}
+	}
+}
+
+const getUID = (schema) => {
+	for(let n = 0; n < schema.length; n++) {
+		const item = schema[n]
+		if(item.type === "UID") {
+			return item.key
+		}
+	}
+	return null
 }
 
 export { create }
